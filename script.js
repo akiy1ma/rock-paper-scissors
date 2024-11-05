9076c040fc1321ad9969071772b02f2fa8c87d06
akiyama <iwanttob2.7lieve@gmail.com>
2024-11-06 02:30:39 +0300

    add logic for playing one round and playing entire game

--- script.js
+++ script.js
@@ -8,17 +8,40 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    return prompt("What would you choose?")
+    return prompt("What would you choose?").toLowerCase()
 }
 
-function updateScore(humanChoise, computerChoise) {
+function updateScore(computerChoise, humanChoise) {
     if (humanChoise == computerChoise) {
         return "draw"
     }
-    else if ((humanChoise == "paper" && computerChoise == "rock") || (humanChoise == "scissors" && computerChoise == "paper") || (humanChoise == "rock" && computerChoise == "scissors")) {
+    else if ((humanChoise === "paper" && computerChoise === "rock") || (humanChoise === "scissors" && computerChoise === "paper") || (humanChoise === "rock" && computerChoise === "scissors")) {
+        humanScore += 1;
         return "human win"
     }
     else {
+        computerScore += 1;
         return "computer win"
     }
 }
+
+
+function startBattle() {
+    return updateScore(getComputerChoice(), getHumanChoice())
+}
+
+function startWar () {
+    for (let i = 1; i <= 5; i++) {
+        startBattle();
+    }
+    if (humanScore > computerScore) {
+        return "humanity won"
+    }
+    else {
+        return "humanity lost"
+    }
+}
+
+console.log(startWar())
+
+//TO-DO: add more interactivity (for now as a text). i want to make a meme edition rock paper scissors with people vs ai war theme
