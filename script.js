102c098d7bc827edc9bc43432b6096583b13cb47
akiyama <iwanttob2.7lieve@gmail.com>
2024-11-11 23:54:21 +0300

    rebuild logic to support basic ui

--- script.js
+++ script.js
@@ -1,47 +1,66 @@
+//TO-DO: add more interactivity (for now as a text). i want to make a meme edition rock paper scissors with people vs ai war theme
+
 let options = ["rock", "paper", "scissors"];
-humanScore = 0;
-computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
+const results = document.querySelector(".results")
 
 
 function getComputerChoice() {
     return options[Math.floor(Math.random() * 3)]
 }
 
-function getHumanChoice() {
-    return prompt("What would you choose?").toLowerCase()
-}
+const rockButton = document.querySelector("#rock-button");
+rockButton.addEventListener("click", () => {
+    startBattle("rock");
+})
 
-function updateScore(computerChoise, humanChoise) {
+const paperButton = document.querySelector("#paper-button");
+paperButton.addEventListener("click", () => {
+    startBattle("paper");
+})
+
+const scissorsButton = document.querySelector("#scissors-button");
+scissorsButton.addEventListener("click", () => {
+    startBattle("scissors");
+})
+
+
+function updateScore(humanChoise, computerChoise) {
     if (humanChoise == computerChoise) {
-        return "draw"
+        results.textContent = "Draw"
     }
     else if ((humanChoise === "paper" && computerChoise === "rock") || (humanChoise === "scissors" && computerChoise === "paper") || (humanChoise === "rock" && computerChoise === "scissors")) {
         humanScore += 1;
-        return "human win"
+        if (humanScore + computerScore === 5) {
+            checkResult();
+        }
+        else{
+            results.textContent = "Human win"
+        }
+        
     }
     else {
         computerScore += 1;
-        return "computer win"
+        if (humanScore + computerScore === 5) {
+            checkResult();
+        }
+        else{
+            results.textContent = "Computer win"
+        }
     }
 }
 
 
-function startBattle() {
-    return updateScore(getComputerChoice(), getHumanChoice())
+function startBattle(humanChoise) {
+    return updateScore(humanChoise, getComputerChoice())
 }
 
-function startWar () {
-    for (let i = 1; i <= 5; i++) {
-        startBattle();
-    }
+function checkResult() {
     if (humanScore > computerScore) {
-        return "humanity won"
+        results.textContent = "humanity won"
     }
     else {
-        return "humanity lost"
+        results.textContent = "humanity lost"
     }
 }
-
-console.log(startWar())
-
-//TO-DO: add more interactivity (for now as a text). i want to make a meme edition rock paper scissors with people vs ai war theme
